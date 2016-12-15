cde528bd9f2cb079dabba134de73b11353bce6cb
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-12-15 12:29:11 -0600

    JQUERY YYYYYYY

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,1 +1,27 @@
-console.log('hello, world');
+$('document').ready(function(){
+  console.log('DOCUMENT IS READY');
+
+  // WHEN CLICKING ON ANSWERS
+  $('.alert').on('click', function(){
+    $(this).addClass('clicked');
+
+    // WHEN CLICKING CORRECT ANSWER
+    if($(this).hasClass('correct')){
+      clickedCorrect();
+    }
+    // WHEN CLICKING INCORRECT ANSWER
+    else{
+      clickedIncorrect();
+    }
+  })
+});
+
+// FUNCTION ZONE WOOOOOOOOOOOOOOOOOO
+function clickedCorrect(){
+  console.log('removing lame class');
+  $(this).removeClass('alert-info');
+}
+
+function clickedIncorrect(){
+  console.log('incorrect answer')
+}
