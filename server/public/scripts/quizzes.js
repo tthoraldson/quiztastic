b174a62d6b6dbc4504ceacf0c366551cf8c6707c
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-01-24 17:44:47 -0600

    dom working with getQuiz route

--- server/public/scripts/quizzes.js
+++ server/public/scripts/quizzes.js
@@ -1,16 +1,24 @@
 $('document').ready(function(){
-  console.log('document is ready');
   getQuizzes();
 });
 
 // function fun zone
 function getQuizzes(){
-  console.log('getQuizzes() started');
   $.ajax({
     type: 'GET',
     url: '/getQuiz',
     success: function(response){
-      console.log(response);
+      //console.log(response);
+
+      for(i = 0; i < response.length; i++)
+      $('.content').append('<div class="col-sm-6 col-md-4">'
+      + '<div class="thumbnail">'
+      + '<div class="caption">'
+      + '<h3>' + response[i].title + '</h3>'
+      + '<p>' + response[i].description + '</p>'
+      + '<p><a href="#" class="btn btn-default" role="button">Take Quiz</a></p>'
+      + '</div></div></div>'
+    );
     }
   });
 }
